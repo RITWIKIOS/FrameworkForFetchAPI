277132ed33b279eadb9a1611310eec7a1a1604b0
RITWIKIOS <ritwikghosh22@gmail.com>
2024-05-28 15:18:58 +0530

    Add files via upload
    initial commit

---
+++ Framework/WebServiceResponseVO.swift
@@ -0,0 +1,13 @@
+//
+//  WebServiceResponseVO.swift
+//  Framework
+//
+//  Created by Ritwik Ghosh on 29/11/16.
+//
+
+import Foundation
+
+protocol WebServiceResponseVO
+{
+    func setData(response:Any) -> Void
+}
